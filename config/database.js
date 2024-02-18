199022a4b397961ede1d148204d32b9152a3e0df
Simon <snsimonescobar@gmail.com>
2024-02-18 13:37:11 -0500

    database changed ready for pord

--- config/database.js
+++ config/database.js
@@ -49,22 +49,22 @@ module.exports = ({ env }) => {
     },
     postgres: {
       connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'dpg-cn93lq8cmk4c739u8dc0-a'),
+        connectionString: env('postgres://sql_db_qyc7_user:85QCVLVJQCRe1qoKRNdUGy4sWEYDra9u@dpg-cn93lq8cmk4c739u8dc0-a.oregon-postgres.render.com/sql_db_qyc7'),
+        host: env('DATABASE_HOST', 'dpg-cn93lq8cmk4c739u8dc0-a.oregon-postgres.render.com'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'sql_db_qyc7'),
         user: env('DATABASE_USERNAME', 'sql_db_qyc7_user'),
         password: env('DATABASE_PASSWORD', '85QCVLVJQCRe1qoKRNdUGy4sWEYDra9u'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
+        ssl: env.bool('DATABASE_SSL', true) && {
+          // key: env('DATABASE_SSL_KEY', undefined),
+          // cert: env('DATABASE_SSL_CERT', undefined),
+          // ca: env('DATABASE_SSL_CA', undefined),
+          // capath: env('DATABASE_SSL_CAPATH', undefined),
+          // cipher: env('DATABASE_SSL_CIPHER', undefined),
+          // rejectUnauthorized: env.bool(
+          //   'DATABASE_SSL_REJECT_UNAUTHORIZED',
+          //   true
+          // ),
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
